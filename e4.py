aee6272ef332f81a3314e4bf73b4f8414962b42f
pol.sanchez.7e7 <pol.sanchez.7e7@itb.cat>
2023-11-30 11:38:01 +0100

    .

--- e4.py
+++ e4.py
@@ -12,4 +12,10 @@ o
 NEGRE="██"
 BLANC=" "
 """
-
+for i in range(8):
+    for j in range(8):
+        if (i + j) % 2 == 0:
+            print("⬜️", end=" ")
+        else:
+            print("⬛️", end=" ")
+    print()
